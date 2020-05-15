0bb762bc0d23ae6248972e42886475b387995537
Nagaraju6242 <62198428+Nagaraju6242@users.noreply.github.com>
2020-05-15 19:45:34 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,12 @@
-var s = "Hii, I'm Vinay.This is my page, you can know about me here. You can contact me for any queries. Everything about my favourites have been listed here. Take a look of my website.";
+var s = "Hii, I'm Vinay. This is my page, you can know about me here. You can contact me for any queries. Everything about my favourites have been listed here. Take a look of my website.";
 var audio = new Audio('music/song.mp3');
 var audio_play = 0;
 var n = s.length;
 var count = 0;
 var x = document.getElementById('test');
 function add(){
-    x.innerHTML = s.slice(0,count);
-    count++;
-    if(count == n+1)
+    x.innerHTML = s[count++];
+    if(count == n)
         clearInterval(t);
 }
 
